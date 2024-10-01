4732c698ad458f0475a83b523b6b40dac5cdcb80
crowdbotics-apps <team@crowdbotics.com>
2024-10-01 07:44:48 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,8 @@
+from django.conf import settings
 from django.db import models
+
+
+class Uvbdhd(models.Model):
+    "Generated Model"
+    vdjd = models.BigIntegerField()
+    ewtsy = models.BigIntegerField()
